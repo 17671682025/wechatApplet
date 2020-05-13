06b2a4fa7465423389d14dcd90e6222c4f3324de
glodeneagle <glodeneagle@sina.com>
2020-05-13 14:05:41 +0800

    修改tabBar

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//app.js
+import 'umtrack-wx';
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -35,5 +35,13 @@ App({
   },
   globalData: {
     userInfo: null
+  },
+  umengConfig: {
+    appKey: '5eb98c23978eea078b7e9fda', //由友盟分配的APP_KEY
+    // 是否使用openid进行统计，此项为false时将使用友盟+随机ID进行用户统计。
+    // 使用openid来统计微信小程序的用户，会使统计的指标更为准确，对系统准确性要求高的应用推荐使用OpenID。
+    useOpenid: false,
+    autoGetOpenid: true, // 是否需要通过友盟后台获取openid，如若需要，请到友盟后台设置appId及secret
+    debug: true, //是否打开调试模式
   }
 })
