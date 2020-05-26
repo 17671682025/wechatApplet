03b3b058644adf9ba853309ee3ac0936c0c27f0c
glodeneagle <glodeneagle@sina.com>
2020-05-27 00:52:01 +0800

    完成scroll-view和swiper的属性熟悉

--- pages/component/viewContainer/scrollView/index.js
+++ pages/component/viewContainer/scrollView/index.js
@@ -1,66 +1,98 @@
 // pages/component/viewContainer/scrollView/index.js
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		// 橫向滚动
+		scrollX: false,
+		// 纵向滚动
+		scrollY: true,
+		// 下拉刷新的阈值
+		upperThreshold: 50,
+		// 上拉加载的阈值
+		lowerThreshold: 50,
+		// 设置竖直滚动条的位置
+		scrollTop: 30,
+		// 设置横向滚动条的位置
+		scrollLeft: 50,
+		// 滚动到某个特定的子元素 值为某元素的id
+		scrollIntoView: 'item_5',
+		// 在设置滚动条位置的动画是否使用
+		scrollWithAnimation: true,
+		// ios点击状态栏 安卓双击标题栏时，滚动条回到顶部
+		enableBackToTop: true,
+		// 开启flex布局
+		enableFlex: false,
+		// 是否开启内容变化位置防抖动  ios有效  android参考overflow-anchor
+		scrollAnchoring: false,
+		// 是否开启自定义下拉刷新
+		refresherEnabled: false,
+		// 设置自定义下拉刷新的阈值
+		refresherThreshold: 45,
+		// 设置自定义下拉刷新的默认样式
+		refresherDefaultStyle: 'black',
+		// 设置自定义下拉刷新的背景色
+		refresherBackground: '#FFF',
+		// 设置当前下拉刷新状态 true表示下拉刷新被触发 false表示下拉刷新未触发
+		refresherTriggered: false,
+	},
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+	/**
+	 * 下拉刷新
+	 */
+	bindscrolltoupper: function () {
+		console.log('下拉刷新函数执行');
+	},
+	/**
+	 * 上拉加载
+	 */
+	bindscrolltolower: function () {
+		console.log('上拉加载函数执行');
     },
-
     /**
-     * 生命周期函数--监听页面加载
+     * scroll-view正在滚动中
      */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    bindscroll:function(event) {
+        console.log("scroll-view滚动中",event.detail)
     }
-})
+});
--- pages/component/viewContainer/swiper/index.js
+++ pages/component/viewContainer/swiper/index.js
@@ -1,66 +1,114 @@
 // pages/component/viewContainer/swiper/index.js
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		swiperList: [
+			{
+				itemId: 'swiper_item1',
+				src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2192316936,131704123&fm=26&gp=0.jpg',
+			},
+			{
+				itemId: 'swiper_item2',
+				src: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2213244297,14939061&fm=26&gp=0.jpg',
+			},
+			{
+				itemId: 'swiper_item3',
+				src:
+					'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590518390564&di=9d941b7366c99fcaa5a610854ae87cdc&imgtype=0&src=http%3A%2F%2Fwww.cnarts.net%2Fuploadimages%2Fcweb%2Fservice%2Fysbz%2Fwallpaper%2F2013-10-29%2F20131029154057.jpg',
+			},
+			{
+				itemId: 'swiper_item4',
+				src:
+					'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590518390564&di=d3a3e9c64a9c4ac95a3e763fc72b0792&imgtype=0&src=http%3A%2F%2Fimage3.cnpp.cn%2Fupload%2Fimages%2F20180515%2F11494176405_1200x800.jpg',
+			},
+		],
+		// 是否自动播放
+		autoplay: true,
+		// 是否显示指示面板
+		indicatorDots: true,
+		// 未激活指示点颜色
+		indicatorColor: 'rgba(255,0,255,1)',
+		// 激活指示点颜色
+		indicatorActiveColor: 'rgba(50,205,50,1)',
+		// 当前激活滑块的index
+		current: 1,
+		// 滑块自动切换的时间
+		interval: 1500,
+		// 滑动动画时长
+		duration: 1000,
+		// 是否使用衔接动画
+		circular: true,
+		// 滑动方向是否为竖向
+		vertical: true,
+		// 前边距
+		previousMargin: '20rpx',
+		// 后边距
+		nextMargin: '20rpx',
+		// 同时展示滑块的数目
+		displayMultipleItems: 3,
+		// 是否跳过未显示的滑块布局，设为 true 可优化复杂情况下的滑动性能，但会丢失隐藏状态滑块的布局信息
+		skipHiddenItemLayout: false,
+		// 动画
+		easingFunction: 'linear',
+	},
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-    },
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
-})
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+	/**
+	 * swiper的current发生改变
+	 */
+	bindchange: function (event) {
+		console.log('current发生改变了', event.detail);
+	},
+	/**
+	 * swiper的swiper-item 的位置发生改变
+	 */
+	bindtransition: function (event) {
+		console.log('swiper-item 的位置发生改变了', event.detail);
+	},
+	/**
+	 * swiper的动画结束时会触发
+	 */
+	bindanimationfinish: function (event) {
+		console.log('动画结束了', event.detail);
+	},
+});
