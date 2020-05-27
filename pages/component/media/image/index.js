ef0eb25a458bcc487e6b35d2a88ad3fc08f16664
glodeneagle <glodeneagle@sina.com>
2020-05-27 23:00:39 +0800

    完成image属性熟悉

--- pages/component/media/image/index.js
+++ pages/component/media/image/index.js
@@ -1,66 +1,59 @@
 // pages/component/media/image/index.js
+
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		// 图片地址
+        src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
+        // 本地图片地址
+        // src2:
+		// 图片是否懒加载
+		lazyLoad: true,
+		// 图片加载模式
+        mode: 'widthFix',
+        // 长按识别二维码
+        showMenuByLongPress:true,
+	},
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-    },
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
-})
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+});
