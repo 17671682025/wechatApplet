82f90b53cc1ba292b852e5895c5a8e044a4b94e9
glodeneagle <glodeneagle@sina.com>
2020-06-16 20:34:36 +0800

    组件封装

---
+++ base/component/basicComponent/boldText/index.js
@@ -0,0 +1,35 @@
+// base/component/basicComponent/title/index.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        title:{
+            type:String,
+            value:""
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        title:""
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    },
+    lifetimes:{
+        attached:function() {
+            const { title='' } = this.properties;
+            this.setData({
+                title
+            })
+        }
+        
+    }
+})
