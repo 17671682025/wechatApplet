d531c7b0378cba18c729fb8e8ded0b7b2931221f
glodeneagle <glodeneagle@sina.com>
2020-06-30 00:55:23 +0800

    熟悉相关api

--- pages/interface/device/setKeepScreenOn/index.js
+++ pages/interface/device/setKeepScreenOn/index.js
@@ -1,66 +1,73 @@
 // pages/interface/device/setKeepScreenOn/index.js
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {},
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+        wx.onUserCaptureScreen((res) => {
+			console.log('监听到用户使用系统截屏了', res);
+		});
+        setTimeout(()=>{
+            wx.offUserCaptureScreen((res) => {
+				console.log('取消用户系统截屏监听', res);
+			});
+        },10000)
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-    },
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+	/**
+	 * 保持小程序常量
+	 */
+	setKeepScreenOn:()=>{
+        wx.setKeepScreenOn({
+			keepScreenOn: true,
+			success: (res) => {
+				console.log('调用成功', res);
+			},
+			fail: (error) => {
+				console.log('调用失败', error);
+			},
+			complete: (res) => {
+				console.log('调用完成', res);
+			},
+		});
     }
-})
+});
