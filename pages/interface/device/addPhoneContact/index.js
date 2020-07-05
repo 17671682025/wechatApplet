2d90b8cee29e3d468a7b8f4c7d40d1f7d0919514
glodeneagle <glodeneagle@sina.com>
2020-07-05 15:04:27 +0800

    相关api熟悉

--- pages/interface/device/addPhoneContact/index.js
+++ pages/interface/device/addPhoneContact/index.js
@@ -1,66 +1,95 @@
 // pages/interface/device/addPhoneContact/index.js
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {},
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-    },
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+	/**
+	 * 添加手机联系人
+	 */
+	addPhoneContact:()=>{
+        wx.addPhoneContact({
+			firstName: '甘小号',
+			nickName: '甘甘',
+			photoFilePath: '/base/images/1.jpg',
+			lastName: '甘',
+			middleName: '怡安',
+			remark: '添加测试',
+			mobilePhoneNumber: '17671682023',
+			weChatNumber: '0123',
+			addressCountry: '中国',
+			addressState: '湖北省',
+			addressCity: '武汉',
+			addressStreet: '洪山区',
+			addressPostalCode: '430010',
+			organization: '武汉智一科技',
+			title: '前端工程师',
+			workFaxNumber: '0000',
+			workPhoneNumber: '8610520',
+			hostNumber: '027-12000',
+			email: '852046302@qq.com',
+			url: 'www.baidu.com',
+			workAddressCountry: '中国',
+			workAddressState: '湖北省',
+			workAddressCity: '武汉',
+			workAddressStreet: '洪山区',
+			workAddressPostalCode: '430010',
+			homeFaxNumber: '0000', 
+			homePhoneNumber: '8610520',
+			homeAddressCountry: '中国',
+			homeAddressState: '湖北省',
+			homeAddressCity: '武汉',
+			homeAddressStreet: '洪山区',
+			homeAddressPostalCode: '',
+			success: (res) => {
+				console.log('调用成功', res);
+			},
+			fail: (error) => {
+				console.log('调用失败', error);
+			},
+			complete: (res) => {
+				console.log('调用完成', res);
+			},
+		});
     }
-})
+});
--- pages/interface/device/startBluetoothDevicesDiscovery/index.js
+++ pages/interface/device/startBluetoothDevicesDiscovery/index.js
@@ -1,66 +1,121 @@
 // pages/interface/device/startBluetoothDevicesDiscovery/index.js
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {},
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-    },
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
-})
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+	/**
+	 * 初始化蓝牙设备
+	 */
+	openBluetoothAdapter: () => {
+		wx.openBluetoothAdapter({
+			success: (res) => {
+				console.log('调用成功', res);
+			},
+			fail: (error) => {
+				console.log('调用失败', error);
+			},
+			complete: (res) => {
+				console.log('调用完成', res);
+			},
+		});
+	},
+	/**
+	 * 获取本机蓝牙状态
+	 */
+	getBluetoothAdapterState: () => {
+		wx.getBluetoothAdapterState({
+			success: (res) => {
+				console.log('调用成功', res);
+			},
+			fail: (error) => {
+				console.log('调用失败', error);
+			},
+			complete: (res) => {
+				console.log('调用完成', res);
+			},
+		});
+	},
+	/**
+	 * 开始搜索蓝牙
+	 */
+	startBluetoothDevicesDiscovery: () => {
+		wx.startBluetoothDevicesDiscovery({
+			allowDuplicatesKey: true,
+			success: (res) => {
+				console.log('调用成功', res);
+			},
+			fail: (error) => {
+				console.log('调用失败', error);
+			},
+			complete: (res) => {
+				console.log('调用完成', res);
+			},
+		});
+	},
+	/**
+	 * 发现蓝牙设备
+	 */
+	foundBluetoothDevice: () => {
+		wx.onBluetoothDeviceFound((res) => {
+			console.log('发现蓝牙设备的设备列表', res);
+		});
+	},
+	/**
+	 * 连接蓝牙设备
+	 */
+	createBLEConnection(e) {
+		const ds = e.currentTarget.dataset;
+		const deviceId = ds.deviceId;
+		const name = ds.name;
+		// 开始连接蓝牙设备
+		wx.createBLEConnection({
+			deviceId,
+			success: (res) => {
+				this.getBLEDeviceServices(deviceId);
+			},
+		});
+		// 在连接蓝牙设备后，停止搜索蓝牙。
+		this.stopBluetoothDevicesDiscovery();
+	},
+});
